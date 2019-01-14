fd2d9d1b2944ac911fd240ab172cf3e5f9509051
Do Cao Tri (FSU1.DES) <docaotri211997@gmail.com>
2019-01-14 16:58:37 +0700

    learn API dot net core

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,32 @@
+﻿using CityInfo.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+        public List<CityDTO> Cities { get; set; }
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDTO>()
+            {
+                new CityDTO()
+                {
+                    ID = 1 ,
+                    Name = "HCM",
+                    Description = "HCM city is very large"
+                },
+                 new CityDTO()
+                {
+                    ID = 1 ,
+                    Name = "Ha Noi",
+                    Description = "Ha Noi city is very large"
+                }
+            };
+        }
+    }
+}
